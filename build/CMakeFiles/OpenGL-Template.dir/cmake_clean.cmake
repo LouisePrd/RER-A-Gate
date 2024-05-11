817b49a927ad1f3f2afab83a50204c544afc8b64
LouisePrd <louise.peridy22@gmail.com>
2024-05-11 15:44:26 +0200

    add template

---
+++ build/CMakeFiles/OpenGL-Template.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/Users/louiseperidy/Desktop/Tower-Defense/bin/Debug/OpenGL-Template"
+  "/Users/louiseperidy/Desktop/Tower-Defense/bin/Debug/OpenGL-Template.pdb"
+  "CMakeFiles/OpenGL-Template.dir/src/App.cpp.o"
+  "CMakeFiles/OpenGL-Template.dir/src/App.cpp.o.d"
+  "CMakeFiles/OpenGL-Template.dir/src/GLHelpers.cpp.o"
+  "CMakeFiles/OpenGL-Template.dir/src/GLHelpers.cpp.o.d"
+  "CMakeFiles/OpenGL-Template.dir/src/main.cpp.o"
+  "CMakeFiles/OpenGL-Template.dir/src/main.cpp.o.d"
+  "CMakeFiles/OpenGL-Template.dir/src/utils.cpp.o"
+  "CMakeFiles/OpenGL-Template.dir/src/utils.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGL-Template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/_deps/simpletext-subbuild/CMakeFiles/simpletext-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpletext-populate"
+  "CMakeFiles/simpletext-populate-complete"
+  "simpletext-populate-prefix/src/simpletext-populate-stamp/simpletext-populate-build"
+  "simpletext-populate-prefix/src/simpletext-populate-stamp/simpletext-populate-configure"
+  "simpletext-populate-prefix/src/simpletext-populate-stamp/simpletext-populate-download"
+  "simpletext-populate-prefix/src/simpletext-populate-stamp/simpletext-populate-install"
+  "simpletext-populate-prefix/src/simpletext-populate-stamp/simpletext-populate-mkdir"
+  "simpletext-populate-prefix/src/simpletext-populate-stamp/simpletext-populate-patch"
+  "simpletext-populate-prefix/src/simpletext-populate-stamp/simpletext-populate-test"
+  "simpletext-populate-prefix/src/simpletext-populate-stamp/simpletext-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/simpletext-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
