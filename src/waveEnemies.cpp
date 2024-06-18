a5d8389bb19ef9174e1845a1251e7916dfc5ac59
LouisePrd <louise.peridy22@gmail.com>
2024-06-18 19:36:27 +0200

    adjust stats

--- src/waveEnemies.cpp
+++ src/waveEnemies.cpp
@@ -8,24 +8,26 @@ Wave createWave(int nbEnemies, int waveNumber, std::vector<std::pair<int, int>> 
     if (waveNumber > 5) {
         nbEnemies += rand() % 10; 
     }
+
+    std::srand(static_cast<unsigned int>(std::time(0)));
     
     for (int i = 0; i < nbEnemies; i++) {
         Enemy enemy;
         if (waveNumber < 5) {
-            enemy.health = 100;
-            enemy.speed = rand() % 4 + 1;
-            enemy.loot = rand() % 50;
+            enemy.health = 90;
+            enemy.speed = 0.6 + static_cast<float>(std::rand()) / (static_cast<float>(RAND_MAX / (2 - 0.6)));
+            enemy.loot = rand() % 70;
             enemy.type = 0;
         } else {
             if (rand() % 2 == 0) {
-                enemy.health = 100;
-                enemy.speed = rand() % 4 + 1;
-                enemy.loot = rand() % 30 + 20;
+                enemy.health = 90;
+                enemy.speed = 0.6 + static_cast<float>(std::rand()) / (static_cast<float>(RAND_MAX / (2 - 0.6)));
+                enemy.loot = rand() % 40 + 20;
                 enemy.type = 0;
             } else {
                 enemy.health = 300;
-                enemy.speed = rand() % 3 + 1;
-                enemy.loot = rand() % 50 + 50;
+                enemy.speed = 0.4 + static_cast<float>(std::rand()) / (static_cast<float>(RAND_MAX / (1.5 - 0.4)));
+                enemy.loot = rand() % 70 + 50;
                 enemy.type = 1;
             }
         }
